a1f59dd1d504bece686d0de0dba6170f10630cb1
xiaoMming <575785513@qq.com>
2017-10-20 22:03:50 -0400

    teamWork_6
    test only

---
+++ Assignment_6/src/Business/Business.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author xiaoMming
+ */
+public class Business {
+
+    private ArrayList<Order> orderList;
+    private ArrayList<Person> personList;
+
+    public ArrayList<Order> getOrderList() {
+        ArrayList<Order> tempArr = new ArrayList<Order>();
+        for (Person p : personList) {
+            for (Order order : p.getOrderList()) {
+                tempArr.add(order);
+            }
+        }
+        orderList = tempArr;
+        return orderList;
+    }
+
+}
---
+++ Assignment_6/src/Business/Person.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author xiaoMming
+ */
+public class Person {
+    private ArrayList<Order> orderList;
+
+    public ArrayList<Order> getOrderList() {
+        return orderList;
+    }
+
+    public void setOrderList(ArrayList<Order> orderList) {
+        this.orderList = orderList;
+    }
+    
+}
